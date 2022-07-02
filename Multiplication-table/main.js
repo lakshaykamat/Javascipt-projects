731311a34c1bbd835a80e4249007ae341f7d5ca9
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-07-02 12:42:00 +0530

    all files deletd

--- Multiplication-table/main.js
+++
@@ -1,9 +0,0 @@
-function multiply(){
-    var number;
-    var result = "";
-    number = Number(document.getElementById("number").value);
-    for(i = 1; i<=10;i++){
-        result =   result + "<p>" + number +  " x " +  i  + " = " + number * i  + "</p>";
-    }
-    document.getElementById("result").innerHTML = result;
-}
